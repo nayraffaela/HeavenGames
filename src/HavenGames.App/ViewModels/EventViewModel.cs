b07f1d28b5bc10a65b30bb010e6de4ed362c14bb
Nayara Rafaela da Silva <nayara.raffaela@gmail.com>
2024-09-19 16:48:58 -0300

    Inclusão de propriedades em enventos

--- src/HavenGames.App/ViewModels/EventViewModel.cs
+++ src/HavenGames.App/ViewModels/EventViewModel.cs
@@ -3,8 +3,11 @@
     public class EventViewModel
     {
         public Guid Id { get; set; }
+        public string  Imagem { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public string Location { get; set; }
         public DateTime Date { get; set; }
     }
 }
--- src/HavenGames.Business/Models/Event.cs
+++ src/HavenGames.Business/Models/Event.cs
@@ -9,7 +9,9 @@ namespace HavenGames.Business.Models
     public class Event:BaseEntity
     {
         public string Name { get; set; }
+        public string Imagem { get; set; }
         public string Description { get; set; }
+        public  string Location { get; set; }
         public DateTime Date { get; set; }
         public IEnumerable<Ticket> Tickets { get; set; }
     }
