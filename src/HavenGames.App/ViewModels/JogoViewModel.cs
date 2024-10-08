5fe1962be63ee408d85d151efe5b73df373f0b2c
nayraffaela <nayara.raffaela@gmail.com>
2024-10-08 09:53:10 -0300

    criar jogo view model

---
+++ src/HavenGames.App/ViewModels/JogoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HavenGames.Business.Models;
+
+namespace HavenGames.App.ViewModels
+{
+    public class JogoViewModel
+    {
+        public string Nome { get; set; }
+        public string Plataforma { get; set; }
+        public string Genero { get; set; }
+        public string Imagem { get; set; }
+        public string Descricao { get; set; }
+        public IList<PersonagemViewModel> Personagens { get; set; }
+    }
+}
