a80fcb9a3aec6aa7fae08609f580b2b5b032ef26
nayraffaela <nayara.raffaela@gmail.com>
2024-10-15 11:06:21 -0300

    Inclusão de JS em NAvbar, ajuste em  responsividade

---
+++ src/HavenGames.App/wwwroot/js/hamburgerResponsiveMenu.js
@@ -0,0 +1,13 @@
+﻿<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const hamburger = document.getElementById('hamburger');
+    const magicMenu = document.querySelector('.magic-menu'); // Use querySelector para selecionar pela classe
+
+    if (hamburger && magicMenu) { // Verifica se os elementos existem
+        hamburger.addEventListener('click', function () {
+            magicMenu.classList.toggle('active');
+        });
+        }
+    });
+</script>
+
